678243d90c13779cb78bd5e49ce2190c49cdd973
3lolo <wopozniack@gmail.com>
2018-05-13 08:46:11 +0300

    [HOME] init home

---
+++ presentation/src/main/kotlin/com/pro/anat/oxdictionary/presentation/module_dictionary/home/HomeVM.kt
@@ -0,0 +1,6 @@
+package com.pro.anat.oxdictionary.presentation.module_dictionary.home
+
+import com.pro.anat.oxdictionary.presentation.base.BaseViewModel
+
+class HomeVM : BaseViewModel<HomeView>() {
+}
---
+++ presentation/src/main/kotlin/com/pro/anat/oxdictionary/presentation/module_dictionary/home/HomeView.kt
@@ -0,0 +1,16 @@
+package com.pro.anat.oxdictionary.presentation.module_dictionary.home
+
+import com.pro.anat.oxdictionary.BR
+import com.pro.anat.oxdictionary.R
+import com.pro.anat.oxdictionary.databinding.FragmentHomeBinding
+import com.pro.anat.oxdictionary.presentation.base.BaseFragmentView
+
+class HomeView : BaseFragmentView<FragmentHomeBinding, HomeVM>() {
+
+    override fun getLayoutId() = R.layout.fragment_home
+
+    override fun performDataBinding(databinding: FragmentHomeBinding) {
+        databinding.setVariable(BR.vm, mViewModel)
+        databinding.executePendingBindings()
+    }
+}
